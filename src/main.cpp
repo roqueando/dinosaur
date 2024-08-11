68701f0667b0c5896e31ab4d5aa0bf034049ee1a
Vitor Roque <vitor.roquep@gmail.com>
2024-08-11 15:03:56 -0300

    fix: adding things

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include <core/csv.h>
 #include <filesystem>
+#include <fstream>
+#include <vector>
 
 namespace fs = std::filesystem;
 
-template<std::size_t... index, typename T, typename R>
-bool read_row_helper(std::index_sequence<index...>, T& row, R& r)
+template<std::size_t... Index, typename Row, typename Reader>
+bool read_row_helper(std::index_sequence<Index...>, Row& row, Reader& r)
 {
-  return r.read_row(std::get<index>(row)...);
+  return r.read_row(std::get<Index>(row)...);
 }
 
+template<std::size_t... Index, typename Row>
+void fill_values(std::index_sequence<Index...>, Row& row, std::vector<double>& data)
+{
+  data.insert(data.end(), { std::get<Index>(row)... });
+}
 
 int main()
 {
   fs::path filepath = "/home/omolu/studdo/dinosaur/data/iris.csv";
+
+  std::ifstream p("../../data/iris.csv");
   io::CSVReader<5> csv_reader(filepath);
 
   std::vector<std::string> categorical_columns;
@@ -21,7 +30,19 @@ int main()
 
   using row_type = std::tuple<double, double, double, double, std::string>;
   row_type row;
-  std::cout << "hello there" << std::endl;
+
+  try {
+    bool done = false;
+    while (!done) {
+      done = !read_row_helper(std::make_index_sequence<std::tuple_size<row_type>::value>{}, row, csv_reader);
+      if (!done) {
+        categorical_columns.push_back(std::get<4>(row));
+        fill_values(std::make_index_sequence<4>{}, row, values);
+      }
+    }
+  } catch (const io::error::no_digit& err) {
+    std::cerr << err.what() << "\n";
+  }
 
   return 0;
 }
