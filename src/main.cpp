bfbd4029e961e254a549369562d590093e3fdd7c
Vitor Roque <vitor.roquep@gmail.com>
2024-10-27 12:35:27 -0300

    fix: now is loading when the file is correct

--- src/main.cpp
+++ src/main.cpp
@@ -3,46 +3,27 @@
 #include <filesystem>
 #include <fstream>
 #include <vector>
+#include <dlib/matrix.h>
 
 namespace fs = std::filesystem;
 
-template<std::size_t... Index, typename Row, typename Reader>
-bool read_row_helper(std::index_sequence<Index...>, Row& row, Reader& r)
-{
-  return r.read_row(std::get<Index>(row)...);
-}
-
-template<std::size_t... Index, typename Row>
-void fill_values(std::index_sequence<Index...>, Row& row, std::vector<double>& data)
-{
-  data.insert(data.end(), { std::get<Index>(row)... });
-}
-
 int main()
 {
-  fs::path filepath = "/home/omolu/studdo/dinosaur/data/iris.csv";
+  dlib::matrix<double> data;
 
-  std::ifstream p("../../data/iris.csv");
-  io::CSVReader<5> csv_reader(filepath);
+  std::ifstream file("../../data/iris.csv");
 
-  std::vector<std::string> categorical_columns;
-  std::vector<double> values;
-
-  using row_type = std::tuple<double, double, double, double, std::string>;
-  row_type row;
-
-  try {
-    bool done = false;
-    while (!done) {
-      done = !read_row_helper(std::make_index_sequence<std::tuple_size<row_type>::value>{}, row, csv_reader);
-      if (!done) {
-        categorical_columns.push_back(std::get<4>(row));
-        fill_values(std::make_index_sequence<4>{}, row, values);
-      }
-    }
-  } catch (const io::error::no_digit& err) {
-    std::cerr << err.what() << "\n";
+  if (!file) {
+    std::cerr << "error opening file" << std::endl;
+    return 1;
   }
+
+  file >> data;
+  if (data.size() == 0) {
+    std::cerr << "error on loading csv to matrix" << std::endl;
+  }
+
+  std::cout << data << std::endl;
 
   return 0;
 }
